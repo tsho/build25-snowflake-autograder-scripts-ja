5c9b78c211a7df710a0a868c5e1951856346fa00
Sho Tanaka <4620750+tsho@users.noreply.github.com>
2025-12-14 11:57:02 +0900

    Modify for Build 2025 Japan

--- snowflake-intelligence.sql
+++ snowflake-intelligence.sql
@@ -5,28 +5,28 @@ use schema public;
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI01' as step
  , (select count(*) from snowflake.information_schema.databases 
-   where database_name in ('DASH_DB_SI', 'SNOWFLAKE_INTELLIGENCE')) as actual
+   where database_name in ('DB_SI_JP', 'SNOWFLAKE_INTELLIGENCE')) as actual
  , 2 as expected
  ,'All databases was created successfully!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI02' as step
- , (select count(*) from dash_db_si.information_schema.stages) as actual
+ , (select count(*) from DB_SI_JP.information_schema.stages) as actual
  , 6 as expected
  ,'All stages were created successfully!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI03' as step
- , (select count(*) from dash_db_si.information_schema.tables where table_name in ('MARKETING_CAMPAIGN_METRICS', 'PRODUCTS', 'SALES','SOCIAL_MEDIA', 'SUPPORT_CASES')) as actual
+ , (select count(*) from DB_SI_JP.information_schema.tables where table_name in ('MARKETING_CAMPAIGN_METRICS', 'PRODUCTS', 'SALES','SOCIAL_MEDIA', 'SUPPORT_CASES')) as actual
  , 5 as expected
  ,'All tables were created successfully!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI04' as step
- , (select count(*) from dash_db_si.information_schema.applicable_roles where role_name = 'SNOWFLAKE_INTELLIGENCE_ADMIN') as actual
+ , (select count(*) from DB_SI_JP.information_schema.applicable_roles where role_name = 'SNOWFLAKE_INTELLIGENCE_ADMIN') as actual
  , 1 as expected
  ,'Role for snowflake intelligence admin created successfully!' as description
 );
@@ -40,7 +40,7 @@ select util_db.public.grader(step, (actual = expected), actual, expected, descri
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI06' as step
- , (select count(distinct metadata$filename) from @dash_db_si.retail.semantic_models where metadata$filename = 'marketing_campaigns.yaml') as actual
+ , (select count(distinct metadata$filename) from @DB_SI_JP.retail.semantic_models where metadata$filename = 'marketing_campaigns.yaml') as actual
  , 1 as expected
  ,'Uploaded semantic model file needed for Cortex Analyst successfully!' as description
 );
@@ -54,28 +54,28 @@ select util_db.public.grader(step, (actual = expected), actual, expected, descri
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI08' as step
- , (select count(*) from dash_db_si.information_schema.functions where function_name = 'ROUNDTOWHOLE') as actual
+ , (select count(*) from DB_SI_JP.information_schema.functions where function_name = 'ROUNDTOWHOLE') as actual
  , 1 as expected
  ,'Created Python UDF successfully!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI09' as step
- , (select count(*) from dash_db_si.information_schema.functions where function_name = 'AVGPRICEPERUNITPRODUCTPERSALE') as actual
+ , (select count(*) from DB_SI_JP.information_schema.functions where function_name = 'AVGPRICEPERUNITPRODUCTPERSALE') as actual
  , 1 as expected
  ,'Created SQL UDTF successfully!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI10' as step
- , (select count(*) from dash_db_si.information_schema.views where table_name = 'AVG_PRICE_PER_UNIT_PRODUCT_PER_SALE') as actual
+ , (select count(*) from DB_SI_JP.information_schema.views where table_name = 'AVG_PRICE_PER_UNIT_PRODUCT_PER_SALE') as actual
  , 1 as expected
  ,'Created a new view using UDF/UDTF successfully!' as description
 );
 
 select util_db.public.grader(step, (actual = expected), actual, expected, description) as graded_results from (SELECT
  'BWSI11' as step
- , (select count(*) from dash_db_si.information_schema.tables where table_name = 'PRODUCTS_WITH_AVG_PRICE') as actual
+ , (select count(*) from DB_SI_JP.information_schema.tables where table_name = 'PRODUCTS_WITH_AVG_PRICE') as actual
  , 1 as expected
  ,'Created a new table using the view successfully!' as description
 );
